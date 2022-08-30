af07d593b17895c56c4ed08451b7189eb3fb0133
tybender <79383478+tybender@users.noreply.github.com>
2022-08-30 04:39:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tyler Bender
+ *         tylerbender@unomaha.edu
+ * Date: 2022/29/08
+ *
+ * Hack 1, Github
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Tyler Bender");
+
+    return 0;
+}
